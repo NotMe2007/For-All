c310fbfaf6b34f944b6f25e975d98f41a8bbe6e3
SeneX <erighdaseos@gmail.com>
2026-07-04 13:51:23 +0200

    Add Anime Warriors III loader
    Adds a new `placeId` branch in `MAINLOADER.lua` for Anime Warriors III and loads the corresponding remote script.

--- MAINLOADER.lua
+++ MAINLOADER.lua
@@ -1,7 +1,8 @@
-local placeId = game.PlaceId
 if placeId == 3351674303 then --Driving Empire
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/c40fe1cd7698bc6bc8b1f96169a26d734c0ea64501fbafff675804f85d48d7cc/download"))()
 elseif placeId == 77085202503540 then -- Grow a Garden 2
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2d7044aa2160636f187a0fc3a14634598d613b62843cdf4648d083c43e092883/download"))()
+elseif placeId == 18923620224 then -- Anime Warriors III
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/49ed2b30735c5526699617d373a80e49f34504b13606b3d7a92b5746b0844f4c/download"))()
 else print("No script found for this placeId: " .. placeId) 
 end
