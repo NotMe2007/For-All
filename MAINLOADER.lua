97b34491e92773921f2830b43c6dec3473f8c80d
SeneX <erighdaseos@gmail.com>
2026-07-04 13:55:29 +0200

    Add loader routes for more games
    Maps several new placeIds to the MentalHub loader for Farm a Fish, Waste Time, Hatch 1 Million Chicken, Watch Number Go Up, Break Your Bones, and PetSim99. Also updates the fallback message to use a generic "No script found for this game."

--- MAINLOADER.lua
+++ MAINLOADER.lua
@@ -4,5 +4,17 @@ elseif placeId == 77085202503540 then -- Grow a Garden 2
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2d7044aa2160636f187a0fc3a14634598d613b62843cdf4648d083c43e092883/download"))()
 elseif placeId == 18923620224 then -- Anime Warriors III
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/49ed2b30735c5526699617d373a80e49f34504b13606b3d7a92b5746b0844f4c/download"))()
-else print("No script found for this placeId: " .. placeId) 
+elseif placeId == 91296119701853 or placeId == 103967585774012 then -- farm a fish
+    loadstring(game:HttpGet("https://mentalhub.me/loader"))()
+elseif placeId == 129548273770183 or placeId == 139877050346809 or placeId == 92050243909420 or placeId == 99207780794303 then -- wastetime
+    loadstring(game:HttpGet("https://mentalhub.me/loader"))()
+elseif placeId == 112781992170158 then -- hatch1millionchicken
+    loadstring(game:HttpGet("https://mentalhub.me/loader"))()
+elseif placeId == 78491332555415 then -- watchnumbergoup
+    loadstring(game:HttpGet("https://mentalhub.me/loader"))()
+elseif placeId == 123821081589134 then -- BreakYourBones
+    loadstring(game:HttpGet("https://mentalhub.me/loader"))()
+elseif placeId == 8737899170 or placeId == 3317771874 or placeId == 16498369169 or placeId == 17503543197 or placeId == 140403681187145 or placeId == 15502339080 or placeId == 15588442388 or placeId == 13764885284 or placeId == 95635359880599 or placeId == 119454325063278 or placeId == 99703032952567 or placeId == 131952481663528 or placeId == 90139951126997 then -- PetSim99
+    loadstring(game:HttpGet("https://mentalhub.me/loader"))()
+else print("No script found for this game.")
 end
